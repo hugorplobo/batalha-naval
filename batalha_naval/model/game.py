87a5040d18bca11d5564dff6a7f94cbbde7601dd
Antônio Hugo <hugorplobo@gmail.com>
2023-05-17 20:37:29 -0300

    feat: add game class

---
+++ batalha_naval/model/game.py
@@ -0,0 +1,16 @@
+from typing import Tuple
+from batalha_naval.model.player import Player
+
+
+class Game:
+    def __init__(self, name1: str, name2: str) -> None:
+        self.__player1 = Player(name1)
+        self.__player2 = Player(name2)
+        self.__current_player = self.__player1
+        self.__next_player = self.__player2
+    
+    def switch_turn(self) -> None:
+        self.__player1, self.__player2 = self.__player2, self.__player1
+    
+    def play_turn(self, pos: Tuple[int, int]) -> None:
+        self.__next_player.receive_shot(pos)
