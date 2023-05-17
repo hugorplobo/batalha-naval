2ef59e7f7b2a518c4b40f0d90cd01fea9b977c3b
Antônio Hugo <hugorplobo@gmail.com>
2023-05-17 20:35:10 -0300

    feat: update player class with new methods

--- batalha_naval/model/player.py
+++ batalha_naval/model/player.py
@@ -5,11 +5,24 @@ from batalha_naval.model.ship import Ship
 
 class Player:
     def __init__(self, name: str) -> None:
-        self.name = name
-        self.board = Board()
+        self.__name = name
+        self.__remaining_cells = 14
+        self.__board = Board()
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def remaining_cells(self):
+        return self.__remaining_cells
 
     def place_ship(self, ship: Ship):
-        self.board.place_ship(ship)
+        self.__board.place_ship(ship)
 
-    def receive_shot(pos: Tuple[int, int]):
-        pass
+    def receive_shot(self, pos: Tuple[int, int]):
+        if self.__board.receive_shot(pos):
+            self.__remaining_cells -= 1
+
+    def has_lost(self):
+        return self.__remaining_cells == 0
