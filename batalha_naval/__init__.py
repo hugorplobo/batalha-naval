ec26df4c8b70b6cd5001ad8053e9f9a2a421fd46
Antônio Hugo <hugorplobo@gmail.com>
2023-05-18 03:27:54 -0300

    feat: init main game loop

--- batalha_naval/__init__.py
+++ batalha_naval/__init__.py
@@ -1,5 +1,17 @@
+from batalha_naval.view.game import GameView
+
+
 def start():
-    print("Batalha Naval")
+    view = GameView()
+    view.show_placing_board(1)
+    view.show_placing_board(2)
+
+    while True:
+        view.show_boards()
+        view.ask_turn()
+
+    input()
+    
 
 
 if __name__ == "__main__":
