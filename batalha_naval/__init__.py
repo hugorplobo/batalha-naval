3e6fb8d98e27025222eb12d283d995faa1fb2bf9
Antônio Hugo <hugorplobo@gmail.com>
2023-05-19 16:37:59 -0300

    feat: add exception handler for keyboard interruption

--- batalha_naval/__init__.py
+++ batalha_naval/__init__.py
@@ -1,20 +1,23 @@
 from batalha_naval.view.game import GameView
-
+import sys
 
 def start():
-    view = GameView()
-    view.show_placing_board(1)
-    view.show_placing_board(2)
+    try:
+        view = GameView()
+        view.show_placing_board(1)
+        view.show_placing_board(2)
 
-    while True:
-        view.show_boards()
-        view.ask_turn()
-        if view.game.player_1.has_lost():
-            view.show_end_game(view.game.player_2)
-            break
-        elif view.game.player_2.has_lost():
-            view.show_end_game(view.game.player_1)
-            break
+        while True:
+            view.show_boards()
+            view.ask_turn()
+            if view.game.player_1.has_lost():
+                view.show_end_game(view.game.player_2)
+                break
+            elif view.game.player_2.has_lost():
+                view.show_end_game(view.game.player_1)
+                break
+    except KeyboardInterrupt:
+        sys.exit(1)
 
 
 if __name__ == "__main__":
